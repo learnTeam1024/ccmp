1fd0befd8a6f92f09daf4cfb654f66114fe5605b
13673639757 <599163126@qq.com>
2016-11-29 19:26:59 +0800

    '优化部门领导审批'

--- bs-webapp/src/main/java/com/pro/bs/approve/controller/ApproveController.java
+++ bs-webapp/src/main/java/com/pro/bs/approve/controller/ApproveController.java
@@ -8,12 +8,9 @@ import com.pro.bs.service.EmpDepService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,11 +27,6 @@ public class ApproveController {
     private   DepartmentService departmentService;
     @RequestMapping(value="/index.do")
     public  String all(HttpSession session,Model model){
-//        String name=(String)session.getAttribute("name");
-//        Integer depNum=empDepService.seDep(name);
-//        Integer supNum=departmentService.findUp(depNum);
-//        String userName=empDepService.seName(supNum);
-//        model.addAttribute("userName",userName);
         String name=(String) session.getAttribute("name");
         return "menu";
     }
@@ -42,6 +34,10 @@ public class ApproveController {
     public String savee(ApproveModel approveModel,HttpSession session){
         String name=(String) session.getAttribute("name");
         approveModel.setPeopleName(name);
+        approveModel.setThreeCheck("等待审批");
+        if (approveModel.getApprovePrice() * approveModel.getApproveNum()<3000){
+            approveModel.setThreeCheck("审批通过");
+        }
        Integer a=approveService.addApp(approveModel);
         if (a!=1){
             return "hello";
